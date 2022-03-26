dc11fb51090c96b17fc76fd288d473952ed43a9e
Yingkun <bai_register@163.com>
2022-03-26 13:04:48 +0100

    Example of using CompletableFuture.supplyAsync to run task in multiple threads

--- src/main/java/concurrency/CompletableFutureConcurrency.java
+++ src/main/java/concurrency/CompletableFutureConcurrency.java
@@ -1,12 +1,20 @@
+package concurrency;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ThreadLocalRandom;
+import java.util.concurrent.CompletionException;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadPoolExecutor;
+
+import com.google.common.util.concurrent.ThreadFactoryBuilder;
 
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 public class CompletableFutureConcurrency {
+	private final static ThreadPoolExecutor threadpool = (ThreadPoolExecutor) Executors.newFixedThreadPool(5,
+			new ThreadFactoryBuilder().setNameFormat("call-1-%d").build());
 
 	public static void main(String[] args) {
 		try {
@@ -16,45 +24,47 @@ public class CompletableFutureConcurrency {
 			List<Integer> failures = new ArrayList<>();
 			for (int i = 0; i < 10; i++) {
 				int record = i;
-				CompletableFuture<Integer> integerCompletableFuture = performTask(i)
-						.whenComplete(((integer, throwable) -> {
+				CompletableFuture<Integer> integerCompletableFuture = CompletableFuture.supplyAsync(() -> {
+					try {
+						return performTask(record);
+					}
+					catch (Exception e) {
+						throw new CompletionException(e);
+					}
+				}, threadpool)
+						.handle((integer, throwable) -> {
 							if (throwable != null) {
 								failures.add(record);
 							} else {
 								successes.add(record);
 							}
-							//return integer;
-						}));
+							return integer;
+						});
 
 				futures.add(integerCompletableFuture);
 			}
 			CompletableFuture.allOf(futures.toArray(new CompletableFuture[0])).join();
 			log.info("successes: {}", successes);
 			log.info("failures: {}", failures);
-		}catch (Exception e) {
+		}
+		catch (Exception e) {
 			log.error("hmmm, something wrong", e);
 		}
 
 	}
 
-	public static CompletableFuture<Integer> performTask(int i){
-		try{
-			log.info("we are doing {}", i);
-			Thread.sleep(100);
-			if (i == 2) throw new Exception("We're devil");
-			if (i == 8) throw new Exception("We're worse");
-		// generate random number between 0 to 10 and divide I+10 by that to
-			// fail
-			// some
-			// tasks
-			// randomly.
-			return CompletableFuture.completedFuture(i);
-		}catch(Exception e){
-			log.error("exception for {}", i, e);
-			CompletableFuture<Integer> f = new CompletableFuture<>();
-			f.completeExceptionally(e);
-			return f;
+	public static Integer performTask(int i) throws IllegalStateException, InterruptedException {
+
+		log.info("we are doing {}", i);
+		Thread.sleep(100);
+		if (i == 2) {
+			throw new IllegalStateException("We're devil");
 		}
+		if (i == 8) {
+			throw new IllegalStateException("We're worse");
+		}
+
+		return i;
 
 	}
 }
--- src/main/java/concurrency/FlatMapForConcurrency.java
+++
@@ -1,6 +0,0 @@
-package concurrency;
-
-public class FlatMapForConcurrency {
-
-
-}
